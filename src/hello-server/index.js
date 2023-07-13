523f105fae150fc1586de45ac693cb2457c7216a
Victor Martin <vmleon@gmail.com>
2023-07-13 08:01:54 +0200

    hello 1.0.1

--- src/hello-server/index.js
+++ src/hello-server/index.js
@@ -69,7 +69,7 @@ app.get("/", async (req, res) => {
     id: serverId,
     name: jsonPackage.name,
     version: jsonPackage.version,
-    message: "hola",
+    message: "hello",
     auth: jsonAuth,
     japp: jsonJapp,
   });
